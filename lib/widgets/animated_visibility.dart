2d20c0b68a2a1a29c26d5cf598a4c6d93b8ea6b2
LoejeeMan <ldulaugon123@gmail.com>
2026-02-23 01:40:51 +0800

    feat: AnimatedVisible maintainSize property

--- lib/widgets/animated_visibility.dart
+++ lib/widgets/animated_visibility.dart
@@ -9,6 +9,7 @@ class AnimatedVisibility extends StatelessWidget {
     this.slideDuration = const Duration(milliseconds: 500),
     this.opacityDuration = const Duration(milliseconds: 400),
     this.curve = Curves.easeInOutCubic,
+    this.maintainSize = false,
   });
 
   final bool visible;
@@ -18,8 +19,16 @@ class AnimatedVisibility extends StatelessWidget {
   final Duration opacityDuration;
   final Curve curve;
 
+  /// If true, the widget will take up space even when not visible.
+  /// If false, the widget will be removed from the layout when not visible.
+  final bool maintainSize;
+
   @override
   Widget build(BuildContext context) {
+    if (!maintainSize && !visible) {
+      return const SizedBox.shrink();
+    }
+
     return AnimatedSlide(
       duration: slideDuration,
       curve: curve,
