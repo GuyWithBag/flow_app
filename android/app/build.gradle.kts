10a4b96930b54cc033480238353f10888d5bb570
LoejeeMan <ldulaugon123@gmail.com>
2026-03-22 16:23:39 +0800

    fix: Accidental removal of lines of code from global replace code editor function

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,11 @@ import java.util.Properties
 import java.io.FileInputStream
 
 plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
 val keystoreProperties = Properties()
 
